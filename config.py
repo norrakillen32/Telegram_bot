87a59884368e0e15c5df838d59aa696f7f8ee24f
Evgenii Maznyak <NorraKillen@yandex.ru>
2026-01-05 22:04:01 +0200

    12fe1

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8572890476:AAHVRIrKb_8JuZI_gvjWputPWKNE78AxNvU"
+WEBHOOK_URL="telegram-cw6tf3qdl-norrakillens-projects.vercel.app/webhook-endpoint"
+SECRET_TOKEN="7b723ea01983026c22478f5150f1be3d7a200c64"
+
+# Пути к данным
+INTENTS_FILE = "data/intents.json"
